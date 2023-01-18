60a2cbc31d9c8695183b129c76877074a1c82df7
yarinmzrc <yarinmzrc@gmail.com>
2023-01-18 22:20:07 +0200

    change config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `${process.env.VITE_BASE_URL}:3000`,
   plugins: [react()],
   server: {
     port: "5000",
