5906e86fc86fb1a642cd12f9809f747ebce2224b
yarinmzrc <yarinmzrc@gmail.com>
2023-01-20 15:16:59 +0200

    adding env variable to url production

--- client/config.js
+++ client/config.js
@@ -3,6 +3,6 @@ export const config = {
     url: "http://127.0.0.1:5020/",
   },
   production: {
-    url: "https://guarded-stream-67963.herokuapp.com/",
+    url: import.meta.env.VITE_PRODUCTION_URL,
   },
 };
